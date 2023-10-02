d4ce6dfbec71c0d1fba3af8a88fb8ba9ae462ed9
John Nordwall <johnfromchicago@gmail.com>
2023-10-02 11:03:48 -0500

    numbers

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"A circle with a radius of {radius} has an area of {AreaOfCircle(radius)}");
+        }
+
+        public static double AreaOfCircle(double radius)
+
+        { 
+            return Math.PI * Math.Pow(radius, 2); 
+        }
+
+        
+       
+        
+           
+        
+
+
+    }
+}
